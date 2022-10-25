d1b527a9c01fda87c53236a7fb1e19e8dff1ddd3
Hongsi-Taste <kwonwk123@gmail.com>
2022-10-25 23:15:42 +0900

    .

---
+++ app/src/main/java/com/example/basic_camera/galActivity.kt
@@ -0,0 +1,11 @@
+package com.example.basic_camera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class galActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gal)
+    }
+}
